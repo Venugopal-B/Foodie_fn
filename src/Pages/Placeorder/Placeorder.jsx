5d439da3007e0598ec57331adf1491ae8abb0488
Venugopal <venugopalb771@gmail.com>
2024-07-31 19:40:48 +0530

    first Commit

---
+++ src/Pages/Placeorder/Placeorder.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import "./Placeorder.css";
+import { StoreContext } from "../../context/storeContext";
+const Placeorder = () => {
+  const {getTotalCartAmount}=useContext(StoreContext)
+  return (
+    <div className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip-Code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="tel" placeholder="Phone" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Charge</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button >
+            Proceed To PAYMENT
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Placeorder;
